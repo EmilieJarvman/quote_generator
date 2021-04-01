29164587f5225eed3dca0ba2af627958799815bb
Emilie <emilie@jarvman.com>
2021-04-01 19:17:23 +0200

    add function to pick random quote from array

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 
 let apiQuotes = [];
 
+// Show new quote
+function newQuote() {
+    // Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
 // Get Quotes From API
 async function getQuotes() {
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
-        console.log(apiQuotes[12])
+        newQuote();
     } catch (error) {
         // Catch Error Here
     }
