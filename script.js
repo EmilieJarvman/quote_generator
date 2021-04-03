2792e2390f0bf87fbb150b873d1dc9c9a273c57b
Emilie <emilie@jarvman.com>
2021-04-03 10:31:56 +0200

    DOM manipulation for quote

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 
 let apiQuotes = [];
 
@@ -5,7 +11,21 @@ let apiQuotes = [];
 function newQuote() {
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+
+    // Check if Author field is blank and replace with "unknown"
+    if(!quote.author) {
+        authorText.textContent = "Unknown"
+    } else {
+        authorText.textContent = quote.author;    
+    }  
+
+    // Check quote length to determine styling
+    if (quote.text.length > 100) {
+        quoteText.classList.add("long-quote");
+    } else {
+        quoteText.classList.remove("long-quote");
+    }
+    quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
